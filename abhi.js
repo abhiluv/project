a2b08941980670a93f0e658214c8e7aba948bafd
abhiluvchauhan1 <achauhan@dminc.com>
2020-01-23 14:21:50 +0530

    first file

---
+++ abhi.js
@@ -0,0 +1,23 @@
+
+// var ele=document.querySelector("p").style.backgroundColor="purple";
+// console.log(ele);
+// var der= document.getElementById("para");
+// console.log(der);
+// var x = document.getElementsByTagName("p");
+// console.log(x);
+// console.log(typeof(x));
+
+// var x = true;
+// Boolean(x);
+// console.log(x);
+// var age=Number(prompt("age"));
+// switch(true) {
+// case age<18:alert("bhagg yaha se");
+// break;
+// case age>18:alert("vote deke ja");  
+// break;
+// default:alert("dimmag mat kharab kare");
+// }
+var arr = [2, 5, 8, 1, 4]
+arr.sort();
+console.log(arr)
